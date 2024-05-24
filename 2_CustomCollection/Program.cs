e651e7a822e7b64ed01e4167dccea76a9f432624
AndreyXP <andreyapple99@mail.ru>
2024-05-24 04:34:57 +0300

    Добавьте файлы проекта.

---
+++ 2_CustomCollection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2_CustomCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
